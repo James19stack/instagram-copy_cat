afb9722bfe6dcfac1a22e54e82b8217fa0e67a90
jefferson <mirarajames29@gmail.com>
2020-07-15 02:27:05 +0300

    Add the changes made in register folder

--- register/views.py
+++ register/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import RegisterUser
+from .email import send_welcome_mail
 
 # Create your views here.
+def register(request):
+    if request.method=='POST':
+        form=RegisterUser(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            email=form.cleaned_data['email']
+            send_welcome_mail(username,email)
+            
+            messages.success(request,f'Account for created successfuly! You can login.')
+            return redirect('login')
+        
+    else:
+        form=RegisterUser()
+    return render(request,'registration/register.html',{'form':form})
+
